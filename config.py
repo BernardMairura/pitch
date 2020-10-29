eb1d7c0099ec1a995999b2bdd3b5f0facac94ddf
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-10-29 16:16:58 +0300

    changes

---
+++ config.py
@@ -0,0 +1,47 @@
+mport os
+
+ 
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    
+    
+    
+
+
+
+class ProdConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL")
+    
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings 
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bernard:86kerubo19@localhost/pitch'
+    
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bernard:86kerubo19@localhost/pitch'
+
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
+
